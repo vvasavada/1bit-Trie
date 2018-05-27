8e060f78dba2077fd1874335ed6db56bb2577621
Trevor <trhackett@ucla.edu>
2018-05-27 12:00:32 -0700

    timer class as well as IP addresses both for testing

---
+++ timer.h
@@ -0,0 +1,28 @@
+//========================================================================
+// Timer t;                 // create a timer
+// t.start();               // start the timer
+// double d = t.elapsed();  // milliseconds since timer was last started
+//========================================================================
+
+#include <chrono>
+
+class Timer
+{
+  public:
+    Timer()
+    {
+        start();
+    }
+    void start()
+    {
+        m_time = std::chrono::high_resolution_clock::now();
+    }
+    double elapsed() const
+    {   
+        std::chrono::duration<double,std::milli> diff =
+                          std::chrono::high_resolution_clock::now() - m_time;
+        return diff.count();
+    }
+  private:
+    std::chrono::high_resolution_clock::time_point m_time;
+};
